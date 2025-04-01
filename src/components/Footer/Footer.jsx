90b426a3fb1cb88523126dbd32725d9f3269e91e
Anastasia_S <sivokonanastasia1@gmail.com>
2025-04-01 22:46:10 +0300

    ain

--- src/components/Footer/Footer.jsx
+++ src/components/Footer/Footer.jsx
@@ -5,11 +5,12 @@ const Footer = () => {
     <div className={s.container}>
         <div className={s.component}>
             <h3 className={s.h3}>Adress</h3>
-            <p className={s.p}></p>
+            <p className={s.p}>вулиця Верхній Вал, 54, Київ, 02000</p>
         </div>
         <div className={s.component}>
             <h3 className={s.h3}>Opening Hours</h3>
-            <p className={s.p}>Weeks</p>
+            <p className={s.p}>Mon - Fri: 8 AM - 8 PM</p>
+            <p className={s.p}>Sat - Sun: 9 AM - 9 PM</p>
         </div>
         
     </div>
